a97c69bad23eab6b98082be35f7819ea47a3bf76
Nadir <inab.nadir.solicode@gmail.com>
2022-01-21 15:15:04 +0100

    adding all the files

---
+++ main.js
@@ -0,0 +1,87 @@
+
+var tbody = document.querySelector("tbody"); 
+var btn = document.getElementById("btn"); 
+
+$.getJSON("surf.json" , (result)=>{
+    var board = result.surfingBoard
+    
+for(var i =0 ;i<board.length ; i++ ){
+        tbody.innerHTML += `
+        <tr>
+        <td> ${board[i].ID} </td>
+        <td> ${board[i].Designation}  </td>
+        <td> ${board[i].Price}</td>
+        <td> ${board[i].Categorie} </td>
+        <td> ${board[i].Disponibilite[0]}</td>
+        <td>
+        <span>${board[i].Supplier.Adresse}</span>
+        <hr>
+        <span> ${board[i].Supplier.raison}</span>
+        
+        </td>
+        </tr>
+        `
+        }
+
+    
+//  KEY UP EVENT  ===> filtering the table
+
+    $(document).ready(function(){
+        $("#search").on("keyup", function(){
+            searchTable($(this).val()) ;
+            function searchTable(ab){
+                $("#tbody tr").each(function(){
+                    var founded = "false" ;
+                    $(this).each(function(){
+                        if($(this).text().toLowerCase().indexOf(ab.toLowerCase())>=0){
+                            founded = "true"
+                        }
+
+                    })
+                    if(founded == "true"){
+                        $(this).show();
+                    }
+                    else{
+                        $(this).hide() ;
+                    }
+
+                })
+
+            }
+            
+        }
+        )
+    })
+ 
+});
+
+
+///// NAVBAR SCROLL
+var nav = document.querySelector("nav"),
+tbody = document.querySelector("tbody");
+// console.log(tbody)
+
+window.addEventListener("scroll" , ()=>{
+    if(window.pageYOffset>60){
+        nav.classList.add("bg-dark" , "shadow")
+
+    }
+    else {
+        nav.classList.remove("bg-dark" ,"shadow");
+    }
+}) 
+////////// 
+
+// function Searching(value1 ,board ){
+//     var filtredData = []
+
+//     for(var i = 0 ; i<board.length ; i++){
+//         value1 = value1.toLowerCase() ;
+//         var name = board[0] ;
+//         console.log(name)
+
+//     }
+//     return filtredData()
+
+
+// }
